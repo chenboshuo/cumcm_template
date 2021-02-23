bc25edbc2db6027481052eef9bb6e07744bdbe7e
chenboshuo <chenboshuo.hi@163.com>
2021-02-23 21:00:15 +0800

    :sparkles: Windows powershell 脚本

---
+++ thesis/make.ps1
@@ -0,0 +1,53 @@
+function build {
+  param (
+    $fileName
+  )
+
+  xelatex -output-directory="./build" $fileName".tex"
+  bibtex ./build/$fileName
+  xelatex -output-directory="./build" $fileName".tex"
+  xelatex -output-directory="./build" $fileName".tex"
+}
+
+function add_extra {
+  param (
+    $fileName
+  )
+
+  Get-Content main.tex |
+  Select-Object -Skip 8 |
+  Out-File $fileName -Append
+}
+
+
+# make build directory
+mkdir build
+
+# make trees
+cd ../src
+tree |Out-File ../thesis/sections/tree.txt
+cd ../thesis
+
+# make todo
+echo "\documentclass[withoutpreface,bwprint]{cumcmthesis} %去掉封面与编号页" |
+  Out-File todo.tex
+add_extra todo.tex
+build todo
+rm todo.tex
+
+# make print version
+echo "\PassOptionsToPackage{disable}{todonotes} \documentclass{cumcmthesis}" |
+  Out-File print.tex
+add_extra print.tex
+build print
+rm print.tex
+
+# make release version
+echo "\PassOptionsToPackage{disable}{todonotes} \documentclass[withoutpreface,bwprint]{cumcmthesis}" |
+  Out-File release.tex
+add_extra release.tex
+build release
+rm release.tex
+
+
+Read-Host
